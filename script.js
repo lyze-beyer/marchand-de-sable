2e93a135eee0db98f4121cd6182930ec5feb91a1
lyze-beyer <lyze.beyer@gmail.com>
2026-07-15 02:17:46 +0200

    Add phrase display functionality to script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("test");
+const phrases = document.querySelectorAll(".phrase");
+const lune = document.querySelector(".play");
+
+let index = 0;
+
+
+function afficherPhrase() {
+
+    if (index < phrases.length) {
+
+        phrases[index].classList.add("visible");
+
+        setTimeout(() => {
+
+            index++;
+            afficherPhrase();
+
+        }, 6000);
+
+    } else {
+
+        setTimeout(() => {
+
+            lune.classList.add("visible");
+
+        }, 3000);
+
+    }
+
+}
+
+
+afficherPhrase();
